27814a1f8e9ea3573cf6b1f28d21c03082e4c821
ahmad zaki <zaki.ahmad300109@gmail.com>
2025-11-26 15:15:32 +0700

    configure for vercel deployment 2

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,3 @@
 # build_files.sh
-echo "Building project..."
-
-# Create and activate virtual environment
-python3.12 -m venv venv
-source venv/bin/activate
-
-# Install dependencies
-python3.12 -m pip install -r requirements.txt
-
-# Create output directory
-mkdir -p staticfiles_build
-
-# Collect static files
-python3.12 manage.py collectstatic --noinput --clear
-
-echo "Build finished successfully"
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
